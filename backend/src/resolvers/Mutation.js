801cdc27d197874e30fe0eb26166afab76e444e5
Mikhail Tukai <tukaym@gmail.com>
2018-10-29 22:11:09 +0300

    User sing up backend logic

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -37,6 +40,32 @@ const mutations = {
 
     // delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    // has the password
+    const password = await bcrypt.hash(args.password, 10);
+    // create user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+    // create JWT
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set jqwt as a cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // reuturn user to the browser
+    return user;
   }
 };
 
