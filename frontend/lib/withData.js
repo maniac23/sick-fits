929272ff4d2a337c90232e49339be55bfca7c24e
Mikhail Tukai <maniac@MacBook-Pro-Mikhail.local>
2019-04-20 19:24:15 +0300

    updatd for prod

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -5,7 +5,7 @@ import { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === 'development' ? prodendpoint : endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
